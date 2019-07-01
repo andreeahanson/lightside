89e16715073ac0f5ca65d10e9759f81c86b4df25
Jev Forsberg <jev.forsberg@gmail.com>
2019-07-01 16:34:04 -0600

    Add first fetch
    Co-authored-by: Andreea Hanson <andreea.hanson7@gmail.com>
    Co-authored-by: Jev Forsberg <jev.forsberg@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,4 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
 
+var obj;
+
+fetch('https://swapi.co/api/people/?page=1')
+  .then(res => res.json())
+  .then(data => obj = data)
+  .then(() => console.log(obj))
+
 ReactDOM.render(<App />, document.getElementById('root'));
