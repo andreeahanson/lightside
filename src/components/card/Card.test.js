8e8fc6dcd6e98b86525cba4482509df5b606d654
Andreea Hanson <andreea.hanson7@gmail.com>
2019-07-07 15:10:01 -0600

    Test card component
    Co-authored-by: Andreea Hanson <andreea.hanson7@gmail.com>
    Co-authored-by: Jev Forsberg <jev.forsberg@gmail.com>

---
+++ src/components/card/Card.test.js
@@ -0,0 +1,86 @@
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import Card from './Card';
+import samplePerson from '../../sampleDataDirectory/samplePeople';
+import sampleVehicles from '../../sampleDataDirectory/sampleVehicles';
+import samplePlanets from '../../sampleDataDirectory/samplePlanets';
+import sampleFavorites from '../../sampleDataDirectory/sampleFavorites';
+
+describe('Card', () => {
+  let wrapper1;
+  let wrapper2;
+  let wrapper3;
+  let toggleFavoriteMock = jest.fn()
+  let classNameMock = '.crap'
+
+  beforeEach(() => {
+    wrapper1 = shallow(
+      <Card 
+      data={samplePerson.results}
+      favorites={sampleFavorites.results}
+      toggleFavorite = {toggleFavoriteMock}
+      className={classNameMock}
+      />);
+
+    wrapper2 = shallow(
+      <Card 
+      data={sampleVehicles.results}
+      favorites={sampleFavorites.results}
+      toggleFavorite = {toggleFavoriteMock}
+      className={classNameMock}
+      />);
+
+      wrapper3 = shallow(
+        <Card 
+        data={samplePlanets.results}
+        favorites={sampleFavorites.results}
+        toggleFavorite = {toggleFavoriteMock}
+        className={classNameMock}
+        />);
+  });
+
+  it('should match the snapshot', () => {
+    expect(wrapper1).toMatchSnapshot();
+    expect(wrapper2).toMatchSnapshot();
+    expect(wrapper3).toMatchSnapshot();
+  });
+
+  it('should match the snapshot when there are no favorites or data items', () => {
+    wrapper1 = shallow(<Card data={[]} favorites={[]} className={classNameMock}/>);
+
+    expect(wrapper1).toMatchSnapshot();
+  });
+
+  it('should invoke toggleFavorite on click', () => {
+    wrapper1 = shallow(<Card data={[{
+      birth_year: "41.9BBY",
+      created: "2014-12-10T16:20:44.310000Z",
+      edited: "2014-12-20T21:17:50.327000Z",
+      eye_color: "blue",
+      films:["https://swapi.co/api/films/5/", "https://swapi.co/api/films/4/", "https://swapi.co/api/films/6/"],
+      gender: "male",
+      hair_color: "blond",
+      height: "188",
+      homeworld: "https://swapi.co/api/planets/1/",
+      mass: "84",
+      name: "Anakin Skywalker",
+      skin_color: "fair",
+      species: ["https://swapi.co/api/species/1/"],
+      starships: ["https://swapi.co/api/starships/59/", "https://swapi.co/api/starships/65/", "https://swapi.co/api/starships/39/"],
+      url: "https://swapi.co/api/people/11/",
+      vehicles: ["https://swapi.co/api/vehicles/44/", "https://swapi.co/api/vehicles/46/"]
+    }]} 
+    favorites={[]} 
+    className={classNameMock}
+    toggleFavorite = {toggleFavoriteMock}
+    />);
+
+    wrapper1.find('article').simulate('click')
+
+    expect(toggleFavoriteMock).toBeCalled()
+  })
+
+  
+
+});
